7f5ad4fc498ada93afd7b5bf91acb0ea7c1823f1
joaoavilars <joao.avila.rs@gmail.com>
2019-09-26 10:06:44 -0300

    first commit

---
+++ comandos.sql
@@ -0,0 +1,23 @@
+use master
+go 
+
+alter database DBPROD set emergency
+go
+
+dbcc checkdb('DBPROD')
+go
+
+alter database	DBPROD set single_user with rollback
+immediate
+go
+
+dbcc checkdb('DBPROD',repair_allow_data_loss)
+with no_infomsgs, all_errormsgs
+go
+
+alter database DBPROD set multi_user
+go
+
+exec sp_resetstatus 'DBPROD'
+
+EXEC sp_resetstatus DBPROD
